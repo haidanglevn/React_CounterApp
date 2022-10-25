6f2dfa809eed0fd34d5d1add78998b7bd518f843
haidanglevn <haidang.levn@gmail.com>
2022-10-25 13:14:02 +0300

    first commit

---
+++ counter/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Header.js";
+import Main from "./Main.js";
+import Footer from "./Footer.js";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
